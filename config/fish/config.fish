732a6be11b108b09e30aedebbd6f6d61e1f00a58
lutet88 <xhex88jeff@gmail.com>
2024-02-14 22:41:47 -0600

    Update Dotfiles at 2024-02-14 22:41:47

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,11 +3,15 @@ if status is-interactive
 end
 
 fish_config theme choose "Tomorrow Night"
+fish_add_path -m ~/.local/bin
+fish_add_path ~/.lc3
 
 function fish_greeting
     echo Welcome (set_color FF5F05)lutet(set_color normal)!
     echo It is (set_color A386C9; date +%c; set_color normal)
 end
 
-source ~/.bashrc_fish
+zoxide init fish --cmd cd | source 
+
+# source ~/.bashrc_fish
 alias mc=micro
